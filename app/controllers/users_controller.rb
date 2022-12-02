3f27ffd8afcfa78f7327b9b5b342c875519956ea
riringo127 <garirei127@gmail.com>
2022-12-02 16:53:02 +0900

    add:ログイン機能#5

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,6 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.require(:user).permit(:name, :email, :password, :password_confirmation, :role)
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'static_pages#top'
 
   get 'login', to: 'user_sessions#new'
-  get 'login', to: 'user_sessions#create'
+  post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
 
   resources :users, only: %i[new create]
