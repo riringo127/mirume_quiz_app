4bbdfb051db2ae85eb38cbbb3bb331d249d51694
riringo127 <garirei127@gmail.com>
2022-12-04 23:51:32 +0900

    add:フラッシュメッセージ追加#32

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,17 +1,18 @@
 class UserSessionsController < ApplicationController
-    def new; end
+  def new; end
 
-    def create
-        @user = login(params[:email], params[:password])
-        if @user
-            redirect_back_or_to dashboard_path
-        else
-            render :new
-        end
+  def create
+    @user = login(params[:email], params[:password])
+    if @user
+      redirect_back_or_to dashboard_path, success: t('.success')
+    else
+      flash.now[:danger] = t('.fail')
+      render :new
     end
+  end
 
-    def destroy
-        logout
-        redirect_to root_path
-    end
+  def destroy
+    logout
+    redirect_to root_path, success: t('.success')
+  end
 end
