150037c69b5ef52a0d20d19e4d5b2ddcc69c1aa8
riringo127 <garirei127@gmail.com>
2022-12-13 16:45:04 +0900

    ダッシュボードに総クリア数表示#91

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,7 @@
 class DashboardsController < ApplicationController
   def top
     @user = User.find(current_user.id)
+    @count = current_user.correct_answers.select(:quiz_id).distinct.count
+    @quizzes = Quiz.all
   end
 end
