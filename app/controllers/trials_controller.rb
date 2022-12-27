5ede29e8fec563fa0dba48dda5673b8bc605cc52
riringo127 <garirei127@gmail.com>
2022-12-27 10:32:45 +0900

    trial_controllerのリファクタリング

--- app/controllers/trials_controller.rb
+++ app/controllers/trials_controller.rb
@@ -2,6 +2,6 @@ class TrialsController < ApplicationController
   skip_before_action :require_login, only: %i[index correct incorrect]
 
   def index
-    @quizzes = Quiz.find([34,13,95,114,22])
+    @quizzes = Quiz.find(Quiz::TRIAL)
   end
 end
