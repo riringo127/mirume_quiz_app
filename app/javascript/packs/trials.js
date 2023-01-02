6bddb1269bc2408767bc6ce40d119aa58c0aa090
riringo127 <garirei127@gmail.com>
2023-01-02 20:30:33 +0900

    add:総クリア数に結果シェアボタン追加#88

--- app/javascript/packs/trials.js
+++ app/javascript/packs/trials.js
@@ -69,7 +69,7 @@ document.getElementById("twitter-share-button").onclick = function() {
   let text = document.getElementById("tweet-text").innerText;
 
   // オプションパラメータを設定
-  let hashtags = ["パーソナルカラー", "パーソナルカラー色識別"];
+  let hashtags = ["パーソナルカラー", "色識別クイズアプリmirume"];
   let url = encodeURIComponent(location.href)  // location.hrefは今いるURL
 
   // URLを生成して遷移
