aed5d38ef87e40c3299846726a86b53df6fd5c71
riringo127 <garirei127@gmail.com>
2023-01-03 20:55:24 +0900

    add:お問合せフォーム作った#99

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,11 @@
 class StaticPagesController < ApplicationController
-  skip_before_action :require_login, only: %i[top privacy_policy terms_of_service]
+  skip_before_action :require_login, only: %i[top privacy_policy terms_of_service contact_form]
 
   def top; end
 
   def privacy_policy; end
 
   def terms_of_service; end
+
+  def contact_form; end
 end
