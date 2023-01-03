296bc437d0e2446086056a680eee48d59c1a992a
riringo127 <garirei127@gmail.com>
2023-01-03 14:06:44 +0900

    add:プライバシーポリシー作った#97

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,7 @@
 class StaticPagesController < ApplicationController
-  skip_before_action :require_login, only: %i[top]
+  skip_before_action :require_login, only: %i[top privacy_policy]
 
   def top; end
+
+  def privacy_policy; end
 end
