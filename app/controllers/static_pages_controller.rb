e4c1d77a773ed1670c7f869bafb8c0c4523d6bb6
riringo127 <garirei127@gmail.com>
2023-01-03 16:55:16 +0900

    add:利用規約作った#98

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,9 @@
 class StaticPagesController < ApplicationController
-  skip_before_action :require_login, only: %i[top privacy_policy]
+  skip_before_action :require_login, only: %i[top privacy_policy terms_of_service]
 
   def top; end
 
   def privacy_policy; end
+
+  def terms_of_service; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   root 'static_pages#top'
   get 'privacy_policy', to: 'static_pages#privacy_policy'
+  get 'terms_of_service', to: 'static_pages#terms_of_service'
 
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
