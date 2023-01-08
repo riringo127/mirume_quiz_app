b39b12c3c1a8eaf6e1c2a88997491e128ef0a47d
riringo127 <garirei127@gmail.com>
2023-01-09 00:54:53 +0900

    add:管理者機能のユーザーと色のCRUD機能#153

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,7 +2,7 @@ class Admin::UsersController < Admin::BaseController
     before_action :set_user, only: %i[edit show update destroy]
   
     def index
-      @users = User.all
+      @users = User.all.order(created_at: :asc)
     end
   
     def show; end
--- config/routes.rb
+++ config/routes.rb
@@ -40,5 +40,6 @@ Rails.application.routes.draw do
     post 'login', to: 'user_sessions#create'
     delete 'logout', to: 'user_sessions#destroy'
     resources :users
+    resources :colors
   end
 end
