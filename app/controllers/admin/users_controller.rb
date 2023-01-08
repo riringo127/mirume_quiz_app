9875dcaee5a8e7f3bacfd8d5ac29db0c3e374786
riringo127 <garirei127@gmail.com>
2023-01-08 22:48:10 +0900

    add:user_crud#153

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,35 @@
+class Admin::UsersController < Admin::BaseController
+    before_action :set_user, only: %i[edit show update destroy]
+  
+    def index
+      @users = User.all
+    end
+  
+    def show; end
+  
+    def edit; end
+  
+    def update
+      if @user.update(user_params)
+        redirect_to admin_users_path, success: 'User was successfully updated.'
+      else
+        flash.now['danger'] = 'User update failed.'
+        render :edit
+      end
+    end
+  
+    def destroy
+      @user.destroy!
+      redirect_to admin_users_path, success: 'User was successfully destroyed.'
+    end
+  
+    private
+  
+    def set_user
+      @user = User.find(params[:id])
+    end
+  
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation, :name, :role)
+    end
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -39,5 +39,6 @@ Rails.application.routes.draw do
     get 'login', to: 'user_sessions#new'
     post 'login', to: 'user_sessions#create'
     delete 'logout', to: 'user_sessions#destroy'
+    resources :users
   end
 end
