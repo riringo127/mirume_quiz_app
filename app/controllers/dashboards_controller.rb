799ca37e7ccdc1f576974fc13da41e353659d5c4
riringo127 <garirei127@gmail.com>
2023-01-24 15:24:22 +0900

    fix:総クリア数の取得方法修正#180

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,6 @@
 class DashboardsController < ApplicationController
   def top
-    @count = current_user.correct_answers.select(:quiz_id).distinct.count
-    @quizzes = Quiz.where(quiz_format: 0)
+    @count = current_user.correct_answers.joins(quiz: :color).select(:color_id).distinct.count
+    @colors = Color.all
   end
 end
